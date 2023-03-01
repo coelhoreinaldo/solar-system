36be14b47948d4f91e11cebebe49d743243d7870
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-01 17:54:22 -0300

    feat: planets and background

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './SolarSystem.css';
 
 export default class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div data-testid="planet-card" className="planet-card">
         <p data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <img
+          className="planet-image"
+          src={ planetImage }
+          alt={ `Planeta ${planetName}` }
+        />
       </div>
     );
   }
