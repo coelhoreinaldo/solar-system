9a7fce568fcfb0c14cf79c4bf4d20a2c25eb8b8d
coelhoreinaldo <scoelhor7@gmail.com>
2023-03-01 17:59:41 -0300

    feat: title center

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../App.css';
 
 export default class Title extends React.Component {
   render() {
     const { headline } = this.props;
     return (
-      <h2>{headline}</h2>
+      <h2 className="title">{headline}</h2>
     );
   }
 }
